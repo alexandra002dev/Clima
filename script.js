b9e2e78916e87ee8a2147787150490229b4070d6
alexandra002dev <alexandramarkess@gmail.com>
2023-10-31 16:07:40 -0300

    atualização css, script

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const button = document.querySelector(".material-symbols-outlined");
 let horaTex = document.querySelector("#hora"); 
 let minutoTex = document.querySelector("#minuto");
 let segundoTex = document.querySelector("#segundo");
+let enter = document.querySelector("#search");
+let aviso = document.querySelector(".right p");
 const key = "b74023e8fa691037aaa1c471c00e4782";
 
 function relogio(){
@@ -27,15 +29,26 @@ function uploadTela(dados){
     document.querySelector(".temperaturaAtual p").innerHTML = `${Math.floor(dados.main.temp)} ºC`;
     document.querySelector(".humidadePorc").innerHTML = `${Math.floor(dados.main.humidity)} %`;
     document.querySelector(".temperaturaAtual span").innerHTML = dados.weather[0].description;
+    document.querySelector(".ventoPct").innerHTML = dados.wind.speed;
     document.querySelector('.temperaturaAtual img').src= `http://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
-    console.log(dados)
-    console.log(data)
-    
+    console.log(dados)  
 };
 
 async function buscarCidade(cidade){
-    let dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&units=metric&lang=pt_br`).then(resposta => resposta.json());
-    uploadTela(dados);
+    if(cidade != ""){
+        aviso.innerHTML = "";
+        let dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&units=metric&lang=pt_br`).then(resposta => resposta.json());
+        
+        if(dados.cod === 200){
+            uploadTela(dados);
+            aviso.innerHTML = "";
+        }else{
+            avisar("Não encontramos essa localização");
+        }
+
+    }else{
+        avisar("Campo vazio!");
+    }
 }
 
 function pesquisar(){
@@ -43,8 +56,19 @@ function pesquisar(){
     buscarCidade(cidade);
 };
 
+function avisar(msg){
+    aviso.innerHTML = msg
+};
+
 
 button.addEventListener("click", pesquisar);
+document.addEventListener("keypress", function(e) {
+    if(e.key === 'Enter') {
+        let btn = document.querySelector(".material-symbols-outlined"); 
+      btn.click();
+    }
+  });
+
 setInterval(relogio,1000);
 
 function fixZero(time) {
